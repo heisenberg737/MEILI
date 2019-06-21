35a5b68f400bbb232ba926eec28a1bb334907c59
heisenberg737 <gokulbhasky@gmail.com>
2019-06-22 00:18:01 +0530

    Designed Contact Us and Home and adjuusted Navigation Drawer

--- app/src/main/java/global/meili/meili/Intro.java
+++ app/src/main/java/global/meili/meili/Intro.java
@@ -1,9 +1,11 @@
 package global.meili.meili;
 
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -17,61 +19,84 @@ public class Intro extends AppCompatActivity {
    private Toolbar toolbar;
    private DrawerLayout drawerLayout;
    private NavigationView navigationView;
+   FragmentManager fragmentManager1=getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("About Us");
 
         final ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_drawer);
 
-        drawerLayout=findViewById(R.id.drawer);
+        drawerLayout=findViewById(R.id.intro);
         navigationView=findViewById(R.id.navigation);
+
+
+        if(findViewById(R.id.intro)!=null)
+        {
+            if(savedInstanceState!=null)
+                return;
+
+        }
+        FragmentTransaction fragmentTransaction1=fragmentManager1.beginTransaction();
+        fragmentTransaction1.add(R.id.MainContent,new home(),null).commit();
+
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Intent intent;
+                Fragment fragment=new home();
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
                 switch (menuItem.getItemId())
                 {
                     case R.id.home:
                         menuItem.setChecked(true);
-                        displaymessage("Home Under development");
+                        getSupportFragmentManager().popBackStack();
+                        toolbar.setTitle("Home");
                         drawerLayout.closeDrawers();
-                        return true;
+                        fragment=new home();
+                        break;
                     case R.id.contact:
                         menuItem.setChecked(true);
-                        intent=new Intent(Intro.this,contactus.class);
-                        startActivity(intent);
-                        finish();
+                        getSupportFragmentManager().popBackStack();
+                        fragment=new contact_us();
+                        toolbar.setTitle("Contact Us");
                         drawerLayout.closeDrawers();
-                        return true;
+                        break;
                     case R.id.login:
                         menuItem.setChecked(true);
                         displaymessage("Login Under development");
+                        toolbar.setTitle("Log In");
                         drawerLayout.closeDrawers();
-                        return true;
+                        break;
                     case R.id.profile:
                         menuItem.setChecked(true);
                         displaymessage("Profile Under development");
+                        toolbar.setTitle("Profile");
                         drawerLayout.closeDrawers();
-                        return true;
+                        break;
                     case R.id.signup:
                         menuItem.setChecked(true);
                         displaymessage("Sign up Under development");
+                        toolbar.setTitle("Sign Up");
                         drawerLayout.closeDrawers();
-                        return true;
+                        break;
                     case R.id.guides:
                         menuItem.setChecked(true);
                         displaymessage("Guides Under development");
+                        toolbar.setTitle("Guides");
                         drawerLayout.closeDrawers();
-                        return true;
+                        break;
                 }
+                fragmentTransaction.replace(R.id.MainContent,fragment,null).commit();
+
+
 
                 return false;
             }
